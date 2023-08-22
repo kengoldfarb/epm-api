adf346a7114ff1897ce1218edfc887bf02d6197a
Ken Goldfarb <hello@kengoldfarb.com>
2023-08-22 15:40:21 -0600

    latest

--- src/types/api.generated.ts
+++ src/types/api.generated.ts
@@ -347,38 +347,6 @@ export namespace GetConfig {
 
 
 
-/** Generate nonce for client to sign and verify a user's wallet address */
-export namespace GetNonce {
-	export interface IPathParams {}
-
-	export const path = () => `/api/1.0/getNonce`
-
-	export const method = HttpMethod.Get
-
-	export interface IQueryParams {
-		/** The address that will be signing */
-		address: string
-	}
-
-	export interface IRequestBody {}
-
-	export interface IResponseBody extends IApiResponseBody {
-		nonce: string
-	}
-
-	export interface IDefinition {
-		pathParams: IPathParams
-		queryParams: IQueryParams
-		requestBody: IRequestBody
-		responseBody: IResponseBody
-	}
-
-	export type Response = IResponseBody | IError
-}
-
-
-
-
 /** Log in a user. */
 export namespace Login {
 	export interface IPathParams {}
@@ -416,7 +384,6 @@ export namespace Login {
 
 	export type Response = IResponseBody | IError
 }
-
 
 
 
